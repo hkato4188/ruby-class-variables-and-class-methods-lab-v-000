092897190aeef0e7d65c27b03a615e49c3534995
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-17 18:20:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,9 @@ class Song
   
   def self.genre_count
     genre_array = Hash.new
-    @@genres.each{|g| genre_array[g] += 1}
+    @@genres.each do |g| 
+      genre_array[g] += 1
+    end
     genre_array
   end
       
