75e24397a15ce8ccf756e458aad1977e78a914d3
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-17 18:00:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,16 @@
 class Song 
   
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end  
+  
 end
+
+
 
 
 
